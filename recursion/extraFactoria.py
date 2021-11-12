a07780bc64b5da41bba7c6dbbc25f1a4f8bfd090
sualah-mtn-gh <88894532+sualah-mtn-gh@users.noreply.github.com>
2021-11-12 17:08:17 +0000

    chore(app):added more data sctructures including trees, linked list etc

---
+++ recursion/extraFactoria.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'extraLongFactorials' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def extraLongFactorials(n):
+    # Write your code here
+    
+    if n in [0,1]:
+        print(1)
+    else:
+        fact = n * 
+        print(2)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    extraLongFactorials(n)
